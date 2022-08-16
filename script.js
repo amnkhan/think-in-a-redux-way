b58e092bd1c4e074c1a593013ef3891520d3813c
Al Amin Khan <amnkhan.me@gmail.com>
2022-08-16 17:47:27 +0600

    Lesson 1.1 done

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-// select dom elements
+// select elements
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
-const counter2El = document.getElementById("counter2");
-const increment2El = document.getElementById("increment2");
-const decrement2El = document.getElementById("decrement2");
+// state
+let counter = 0;
 
-// initial state
-let count = 0;
-let count2 = 0;
+// increment function
+const increment = () => {
+  counter = counter + 1;
+  counterEl.textContent = counter.toString();
+};
 
-// event listeners
-incrementEl.addEventListener("click", () => {
-    count++;
-    counterEl.innerText = count;
-});
+// decrement function
+const decrement = () => {
+  counter = counter - 1;
+  counterEl.textContent = counter.toString();
+};
 
-decrementEl.addEventListener("click", () => {
-    count--;
-    counterEl.innerText = count;
-});
-
-increment2El.addEventListener("click", () => {
-    count2++;
-    counter2El.innerText = count2;
-});
-
-decrement2El.addEventListener("click", () => {
-    count2--;
-    counter2El.innerText = count2;
-});
+// add event listeners
+incrementEl.addEventListener("click", increment);
+decrementEl.addEventListener("click", decrement);
